2b8d81004bba703bb5c64b4218d97b26f129f0e3
Luxoric <Klavkaklavkovich@gmail.com>
2022-06-09 13:15:58 +0300

    Create ZAGLЯNI.cs

---
+++ Assets/LOL/ZAGLЯNI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZAGLЯNI : MonoBehaviour
+{
+    //Мне было лень открывать вайбер, поэтому тут напишу, это файл ля общения будет (мой)
+    //Вопрос к тебе, я забыл че дальше делать?
+}
