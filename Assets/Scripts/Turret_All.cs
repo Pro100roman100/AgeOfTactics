75dcf38048c7b423ad34cd91ec2aebb2c2f77f8d
Luxoric <Klavkaklavkovich@gmail.com>
2022-06-09 12:05:40 +0300

    adding_Turret

---
+++ Assets/Scripts/Turret_All.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_All : Building 
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
