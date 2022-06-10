b15a896d57d5c084cb16e1c9603ef72ceda58f85
Luxoric <Klavkaklavkovich@gmail.com>
2022-06-10 10:55:58 +0300

    KU

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    private GameObject Block;
+    private GameObject Miner;
+    private GameObject Turret;
+    
+
+    private void Awake()
+    {
+        Block = GameObject.Find("Block");
+        Miner = GameObject.Find("Miner");
+        Turret = GameObject.Find("Turret");
+        Block.SetActive(false);
+        Miner.SetActive(false);
+        Turret.SetActive(false);
+    }
+
+    private void OnClick()
+    {
+        Block.SetActive(true);
+        Miner.SetActive(true);
+        Turret.SetActive(true);
+    }
+}
